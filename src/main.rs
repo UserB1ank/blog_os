da8d67c8d1ad8456d8059792a3222927a064abfd
NEO <105202277+UserB1ank@users.noreply.github.com>
2026-06-15 15:20:25 +0800

    VGA Text Mode

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
 #![no_main] // 禁用所有 Rust 层级的入口点
 #![no_std]
+
+use core::fmt::write;
 use core::panic::PanicInfo;
 mod vga_buffer;
 static HELLO:&[u8]=b"helloworld";
 
 #[unsafe(no_mangle)] // 不重整函数名
 pub extern "C" fn _start() -> ! {
-
-    vga_buffer::print_something();
+    println!("Hello World");
+    println!("helloworld");
+    // panic!("noo");
     loop {}
 }
 
 /// 这个函数将在 panic 时被调用
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}",info);
     loop {}
 }
