e8229d40df2de5fcbecfc62c4ffd3333ae01c0b5
NEO <105202277+UserB1ank@users.noreply.github.com>
2026-06-18 18:07:37 +0800

    内核测试

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,31 @@
-#![no_main] // 禁用所有 Rust 层级的入口点
+
 #![no_std]
+#![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(crate::test_runner)]
+#![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use x86_64::instructions::port::Port;
+use blog_os::println;
 
-mod vga_buffer;
-
-
-#[unsafe(no_mangle)] // 不重整函数名
+#[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    println!("Hello World");
-
-
+    println!("Hello World{}", "!");
     #[cfg(test)]
     test_main();
-    panic!("noo");
     loop {}
 }
 
-#[test_case]
-fn trivial_assertion() {
-    print!("trivial assertion... ");
-    assert_eq!(1, 1);
-    println!("[ok]");
-}
-
-/// 这个函数将在 panic 时被调用
+/// This function is called on panic.
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("{}",info);
+    println!("{}", info);
     loop {}
 }
 
 #[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-    }
-    exit_qemu(QemuExitCode::Failed);
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    blog_os::test_panic_handler(info);
 }
-
-#[derive(Debug,Clone,Copy,PartialEq,Eq)]
-#[repr(u32)]
-pub enum QemuExitCode{
-    Success=0x10,
-    Failed=0x11,
-}
-
-pub fn exit_qemu(exit_code:QemuExitCode){
-    use x86_64::instructions::port::Port;
-    unsafe {
-        let mut port=Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-
